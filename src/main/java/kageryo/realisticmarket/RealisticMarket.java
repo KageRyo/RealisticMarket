17478454aeb74346e096ed799d42a3ea16083ed4
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-10-19 13:45:38 +0800

    初始化環境

---
+++ src/main/java/kageryo/realisticmarket/RealisticMarket.java
@@ -0,0 +1,17 @@
+package kageryo.realisticmarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RealisticMarket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
