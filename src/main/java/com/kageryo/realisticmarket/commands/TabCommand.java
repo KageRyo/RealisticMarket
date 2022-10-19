26a2ca7026dcf9495f8647e6bbf0b8ddfc9d48de
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-10-19 15:27:10 +0800

    增加 TabCommand

---
+++ src/main/java/com/kageryo/realisticmarket/commands/TabCommand.java
@@ -0,0 +1,19 @@
+package com.kageryo.realisticmarket.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabCommand implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args){
+        List<String> commandList = new ArrayList<>();
+        if(args.length == 1){
+            commandList.add("test");
+        }
+        return commandList;
+    }
+}
