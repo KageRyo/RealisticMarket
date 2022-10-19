3b8c0a699224c49c368844372cc2186aaa41875e
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-10-19 15:11:11 +0800

    建立Command

---
+++ src/main/java/com/kageryo/realisticmarket/commands/MainCommand.java
@@ -0,0 +1,13 @@
+package com.kageryo.realisticmarket.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class MainCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+
+        return false;
+    }
+}
