fdc51905031a0a381eb2aca49d614ab2d8cff452
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-10-19 15:25:20 +0800

    增加測試用指令

--- src/main/java/com/kageryo/realisticmarket/commands/MainCommand.java
+++ src/main/java/com/kageryo/realisticmarket/commands/MainCommand.java
@@ -1,13 +1,24 @@
 package com.kageryo.realisticmarket.commands;
 
+import com.kageryo.realisticmarket.RealisticMarket;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MainCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-
+        if(args.length==1){
+            if(args[0].equals("test")){
+                String testMessage="測試訊息";
+                RealisticMarket.realisticMarket.getLogger().info(testMessage);
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
+                    player.sendMessage(testMessage);
+                }
+            }
+        }
         return false;
     }
 }
