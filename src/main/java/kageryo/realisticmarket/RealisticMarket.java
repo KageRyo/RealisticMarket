e3ccf3416080b052c57502b0f4f68f70164474b4
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-10-19 14:37:15 +0800

    初始化 VauitAPI

--- src/main/java/kageryo/realisticmarket/RealisticMarket.java
+++ src/main/java/kageryo/realisticmarket/RealisticMarket.java
@@ -1,32 +1,96 @@
 package kageryo.realisticmarket;
 
+import java.util.logging.Logger;
+
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RealisticMarket extends JavaPlugin {
-    public static RealisticMarket realisticMarket;
-    public static Economy econ = null;
-    public static Permission perms = null;
-    public static Chat chat = null;
+
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
+    @Override
+    public void onDisable() {
+        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        realisticMarket = this;
-        this.getLogger().info("RealisticMarket Plugin By.K6Dev@DreamFuture-Studio"); // 插件歡迎訊息(檢查是否運作用)
-
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            this.getLogger().info("未偵測到經濟系統，請安裝後再試。");
-            // 禁用插件
+        if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
-        }else{
-            this.getLogger().info("經濟系統已啟用。");
+        }
+        setupPermissions();
+        setupChat();
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if(!(sender instanceof Player)) {
+            log.info("Only players are supported for this Example Plugin, but you should not do this!!!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if(command.getLabel().equals("test-economy")) {
+            // Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
+            sender.sendMessage(String.format("You have %s", econ.format(econ.getBalance(player.getName()))));
+            EconomyResponse r = econ.depositPlayer(player, 1.05);
+            if(r.transactionSuccess()) {
+                sender.sendMessage(String.format("You were given %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
+            } else {
+                sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
+            }
+            return true;
+        } else if(command.getLabel().equals("test-permission")) {
+            // Lets test if user has the node "example.plugin.awesome" to determine if they are awesome or just suck
+            if(perms.has(player, "example.plugin.awesome")) {
+                sender.sendMessage("You are awesome!");
+            } else {
+                sender.sendMessage("You suck!");
+            }
+            return true;
+        } else {
+            return false;
         }
     }
+
     public static Economy getEconomy() {
         return econ;
     }
